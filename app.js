0f6d2a5b61addb6c570d8d483b957068fff4b758
igiboi <36388765+igiboi@users.noreply.github.com>
2020-11-30 10:28:16 -0500

    fundamentals of arrow functions es6

---
+++ app.js
@@ -0,0 +1,13 @@
+const sayHello = function() {
+  console.log("Hello");
+}
+
+// Arrow functions
+const sayHello = () => {
+  console.log('Arrow funcitons')
+}
+
+// one line functions 
+
+const sayArrow = console.log("One line Hello"); 
+
